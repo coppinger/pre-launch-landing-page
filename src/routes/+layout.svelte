f6b5a5c053571c04017261d844de26863e05ba7d
Charlie Coppinger <charles.coppinger@gmail.com>
2023-07-30 16:53:59 +1200

    get in basic structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,16 @@
   import "../app.css";
 </script>
 
-<div class="w-screen h-screen flex justify-center items-center bg-blue-950">
-    <slot />
+<div class="w-screen h-screen flex justify-center items-center bg-slate-950 p-3 text-slate-50">
+    <div class="border-[12px] border-slate-700 w-full h-full max-w-7xl px-16 py-12">
+        <header class="flex justify-between w-full">
+            <div class="">Logo</div>
+            <a href="#">Reserve username</a>
+        </header>
+        <slot />
+        <footer>
+            <div class="">Logo</div>
+            <h3>Built for the Builders</h3>
+        </footer>
+    </div> 
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,12 @@
-<h1 class="text-3xl text-blue-500">Hello, World!</h1>
+<section class="flex gap-3 flex-col items-center">
+	<h1 class="text-5xl font-bold">A social network for indie hackers</h1>
+	<p class="text-slate-400 text-xl font-medium">
+		Break through the noise, share your journey, and build your own success story.
+	</p>
+</section>
+<section>
+	<form action="">
+		<input type="text" />
+		<input type="button" value="Test" />
+	</form>
+</section>
